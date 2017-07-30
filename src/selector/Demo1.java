b57c9a581d83f6b1fc3c5fc4da08cb41341ad730
wendrewshay <wendrewshay@sina.com>
2017-07-30 16:57:30 +0800

    NIO和AIO

---
+++ src/selector/Demo1.java
@@ -0,0 +1,34 @@
+package selector;
+
+import java.io.IOException;
+import java.nio.channels.Selector;
+
+public class Demo1 {
+
+	public static void main(String[] args) throws IOException {
+		
+		Selector selector = Selector.open();
+//		channel.configureBlocking(false);
+//		SelectionKey key = channel.register(selector, SelectionKey.OP_READ);
+//		while(true) {
+//		  int readyChannels = selector.select();
+//		  if(readyChannels == 0) continue;
+//		  Set selectedKeys = selector.selectedKeys();
+//		  Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
+//		  while(keyIterator.hasNext()) {
+//		    SelectionKey key1 = keyIterator.next();
+//		    if(key1.isAcceptable()) {
+//		        // a connection was accepted by a ServerSocketChannel.
+//		    } else if (key1.isConnectable()) {
+//		        // a connection was established with a remote server.
+//		    } else if (key1.isReadable()) {
+//		        // a channel is ready for reading
+//		    } else if (key1.isWritable()) {
+//		        // a channel is ready for writing
+//		    }
+//		    keyIterator.remove();
+//		  }
+//		}
+		
+	}
+}
